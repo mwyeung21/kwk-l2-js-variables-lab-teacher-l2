737980bb62d4bdd47ce807105c5a7a941345c07e
Melany Yeung <mwyeung21@gmail.com>
2018-06-20 18:24:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+// #1 is set as scuber 
+// raises error if the companyName is changed
+const companyName = "Scuber"
+
+let mostProfitableNeighborhood = "Chelsea"
+// ^^can use let or var
+// QUESTION: when to use let or var???
+
+let companyCeo = "Susan Smith"
